08b1603a9643d9735035937800b44a2e5317e84a
Serendipity <1789216340lhy@gmail.com>
2021-07-01 16:59:35 +0800

    Add read preprocess data from file module

--- Try on my own/Pix2Pix_Performance.py
+++ Try on my own/Pix2Pix_Performance.py
@@ -14,10 +14,8 @@ import csv
 
 #从硬盘中读取已经预处理完成的CSV文件
 def read_module_from_disk():
-    TRAIN_DATA_LOACTION = ""
-    TEST_DATA_LOCATION = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
+    PreprocessLocation = ""
     
-    train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
-    test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
+    PreprocessPath = tf.keras.utils.get_file("Preprocess.csv", PreprocessLocation)
     np.set_printoptions(precision = 3, suppress = True)
 
