38833db4431764f7cc6ba547a29f66b4fcff8ac7
Serendipity <hl3265@truman.edu>
2021-07-06 15:44:32 +0800

    Try let torch working on the CLion
    Yes, I gave up let it running on VS

--- WaterMarkCore_TorchC/DCT_Watermark.cpp
+++ WaterMarkCore_TorchC/DCT_Watermark.cpp
@@ -6,14 +6,17 @@
 
 void DCT_Watermark::Generator()
 {
-  cv::Mat Input = "/Users/arkia/ComputerScienceRelated/Watermark Faker/Watermark Faker Data/facades/test";
+  cv::Mat Input;
+  cv::Mat Process;
   cv::Mat DCTEDWaterMark;
   
 }
 
 void DCT_Watermark::GetCSVFromDisk()
 {
-
+   std::ifstream infile;
+   infile.open("D:/CS-Related/Watermark Faker/PreprocessDir/Preprocess_B_Image.csv");
+   
 }
 
 void DCT_Watermark::CoreTensorModule()
--- WaterMarkCore_TorchC/DCT_Watermark.h
+++ WaterMarkCore_TorchC/DCT_Watermark.h
@@ -2,9 +2,10 @@
 #ifndef WATERMARKCOREC___DCT_WATERMARK_H
 #define WATERMARKCOREC___DCT_WATERMARK_H
 
-#include<iostream>
+#include <iostream>
 #include <torch/torch.h>
 #include <vector>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 
 class DCT_Watermark
