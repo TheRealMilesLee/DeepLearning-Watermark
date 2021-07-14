26d8075c2243144123a8256f535776c0d9c464ee
Serendipity <hl3265@truman.edu>
2021-07-14 20:31:48 +0800

    Update DFT-de(a lot).py

--- watermarkFakerCore/code_watermark_wang/dft_watermark/DFT-de(a lot).py
+++ watermarkFakerCore/code_watermark_wang/dft_watermark/DFT-de(a lot).py
@@ -3,47 +3,39 @@ import cv2
 import os
 import glob
 
+original_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Source"  # 与水印图片配对的原始图片
+watermarked_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Output"  # 加了水印的图片
+output_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Output"  # 输出提取的水印的地方
 
-def de_fourier_watermark(image, wmed):
-    image_f = np.fft.fft2(image, axes=(0, 1))
-    wmed_f = np.fft.fft2(wmed, axes=(0, 1))
-
-    alpha, i_height, i_width = 5, image.shape[0], image.shape[1]
-
-    result = np.real((wmed_f - image_f) / alpha)
-
+'''
+-------此处是傅里叶变换部分------
+和之前的不同，这次是逆向变换，也就是从频域变换回时域
+'''
+def de_fourier_watermark(OriginalImage, WatermarkedImage):
+    OriginalImage_Fourier = np.fft.fft2(OriginalImage, axes=(0, 1))
+    WatermarkedImage_Fourier = np.fft.fft2(WatermarkedImage, axes=(0, 1))
+    alpha, i_height, i_width = 5, OriginalImage.shape[0], OriginalImage.shape[1]
+    result = np.real((WatermarkedImage_Fourier - OriginalImage_Fourier) / alpha)
     return result
 
-# --------------原始设置--------------------
-# original_path = r"F:\image_database\grey"
-# watermarked_path = r"F:\image_database\grey_watermark_frequency"
-# output_path = r"F:\image_database\gery_dewatermark_test"
-
-original_path = r"F:\image_database\grey"  # 与水印图片配对的原始图片
-watermarked_path = r"F:\image_database\grey_watermark_frequency-backup"  # 加了水印的图片
-output_path = r"F:\image_database\gery_dewatermark_test"  # 输出提取的水印的地方
-
+# 读取与水印匹配的源文件
 originals = glob.glob(os.path.join(original_path, "*.png"))
-watermarkeds = glob.glob(os.path.join(watermarked_path, "*.png"))
+# 读取带水印的图片文件
+Watermarked_Image = glob.glob(os.path.join(watermarked_path, "*.png"))
 
 for order in range(len(originals)):
     print(order)
-
+    # 拆分文件名
     basename, _ = os.path.splitext(os.path.basename(originals[order]))
+    # 为输出图片指定文件名
     watermark_name = basename + "_freWatermark"
-
     sibling_path = os.path.join(watermarked_path, basename + "_freWatermark" + ".png")
-
     if not os.path.exists(sibling_path):
         print("配对的水印图片不存在")
-
-    image = cv2.imread(originals[order], 0)  # 后面加入了0之后，读取的就是单通道的灰度图了
-    wmed = cv2.imread(sibling_path, 0)
-
-    de_watermark = de_fourier_watermark(image, wmed)
-
+    originals = cv2.imread(originals[order], 0)  # 后面加入了0之后，读取的就是单通道的灰度图了
+    Watermarked_Image = cv2.imread(sibling_path, 0)
+    de_watermark = de_fourier_watermark(originals, Watermarked_Image)
     name = os.path.basename(originals[order]).split(".")[0]
     save_path = os.path.join(output_path, name + "_de.png")
     print(save_path)
-
     cv2.imwrite(save_path, de_watermark)
