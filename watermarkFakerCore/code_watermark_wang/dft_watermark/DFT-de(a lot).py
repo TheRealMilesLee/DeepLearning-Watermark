4519a56927b411465c87bd7aae5de5c90b35f487
Serendipity <hl3265@truman.edu>
2021-07-15 11:35:24 +0800

    Still working on the reversing DFT, just can't read in files

--- watermarkFakerCore/code_watermark_wang/dft_watermark/DFT-de(a lot).py
+++ watermarkFakerCore/code_watermark_wang/dft_watermark/DFT-de(a lot).py
@@ -4,13 +4,9 @@ import os
 import glob
 
 original_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Source"  # 与水印图片配对的原始图片
-watermarked_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Output"  # 加了水印的图片
-output_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Output"  # 输出提取的水印的地方
+watermarked_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Output/5_wDFT.png"  # 加了水印的图片
+output_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DFT/Train images/Output/Reverse"  # 输出提取的水印的地方
 
-'''
--------此处是傅里叶变换部分------
-和之前的不同，这次是逆向变换，也就是从频域变换回时域
-'''
 def de_fourier_watermark(OriginalImage, WatermarkedImage):
     OriginalImage_Fourier = np.fft.fft2(OriginalImage, axes=(0, 1))
     WatermarkedImage_Fourier = np.fft.fft2(WatermarkedImage, axes=(0, 1))
@@ -19,10 +15,14 @@ def de_fourier_watermark(OriginalImage, WatermarkedImage):
     return result
 
 # 读取与水印匹配的源文件
-originals = glob.glob(os.path.join(original_path, "*.png"))
+Source_Filename = glob.glob(os.path.join(original_path, "*png"))
+Source = cv2.imread(Source_Filename)
+cv2.imshow('Source File', Source)
 # 读取带水印的图片文件
-Watermarked_Image = glob.glob(os.path.join(watermarked_path, "*.png"))
-
+Watermark_Filename = glob.glob(os.path.join(watermarked_path, "*.png"))
+Watermark = cv2.imread(Watermark_Filename)
+cv2.imshow('Watermarked Image', Watermark)
+'''
 for order in range(len(originals)):
     print(order)
     # 拆分文件名
@@ -32,10 +32,12 @@ for order in range(len(originals)):
     sibling_path = os.path.join(watermarked_path, basename + "_freWatermark" + ".png")
     if not os.path.exists(sibling_path):
         print("配对的水印图片不存在")
-    originals = cv2.imread(originals[order], 0)  # 后面加入了0之后，读取的就是单通道的灰度图了
-    Watermarked_Image = cv2.imread(sibling_path, 0)
-    de_watermark = de_fourier_watermark(originals, Watermarked_Image)
+    NewImage = cv2.imread(originals[order], 0)  # 后面加入了0之后，读取的就是单通道的灰度图了
+    Watermark = cv2.imread(sibling_path, 0)
+    de_watermark = de_fourier_watermark(originals, Watermark)
     name = os.path.basename(originals[order]).split(".")[0]
     save_path = os.path.join(output_path, name + "_de.png")
     print(save_path)
     cv2.imwrite(save_path, de_watermark)
+
+'''
