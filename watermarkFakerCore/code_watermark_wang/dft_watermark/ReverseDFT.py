6f1f21fe4ec68a23d4fe0f16fcc79d355a9559c7
Serendipity <hl3265@truman.edu>
2021-07-20 11:34:45 +0800

    Finished ReverseDFT

--- watermarkFakerCore/code_watermark_wang/dft_watermark/ReverseDFT.py
+++ watermarkFakerCore/code_watermark_wang/dft_watermark/ReverseDFT.py
@@ -6,26 +6,35 @@ import glob
 def de_fourier_watermark(OriginalImage, WatermarkedImage):
     OriginalImage_Fourier = np.fft.fft2(OriginalImage, axes=(0, 1))
     WatermarkedImage_Fourier = np.fft.fft2(WatermarkedImage, axes=(0, 1))
-    alpha, i_height, i_width = 5, OriginalImage.shape[0], OriginalImage.shape[1]
+    alpha = 5, OriginalImage.shape[0], OriginalImage.shape[1]
     result = np.real((WatermarkedImage_Fourier - OriginalImage_Fourier) / alpha)
     return result
 
 original_path = "/Users/arkia/ComputerScienceRelated/Watermark Faker/Watermark Faker Data/DFT/Train images/Source"  # 与水印图片配对的原始图片
 watermarked_path = "/Users/arkia/ComputerScienceRelated/Watermark Faker/Watermark Faker Data/DFT/Train images/Output"  # 加了水印的图片
 output_path = "/Users/arkia/ComputerScienceRelated/Watermark Faker/Watermark Faker Data/DFT/Train images/Output/Reverse"  # 输出提取的水印的地方
+
 # 读取与水印匹配的源文件
 Source_Filename = glob.glob(os.path.join(original_path, "*.png"))
-print(len(Source_Filename))
+
 # 读取带水印的图片文件
 Watermark_Filename = glob.glob(os.path.join(watermarked_path, "*.png"))
-print(len(Watermark_Filename))
+
+#进行反向水印提取
 for order in range(len(Source_Filename)):
-    print(order)
+    # Exception Handling
     if not os.path.exists(watermarked_path):
         print("配对的水印图片不存在")
+    
+    #读入与水印配对的源文件
     Source = cv2.imread(Source_Filename[order], 0)  # 后面加入了0之后，读取的就是单通道的灰度图了
+    
+    # 读入带水印的文件
     Watermark = cv2.imread(Watermark_Filename[order], 0)
+    
+    # Core fourier transfer
     de_watermark = de_fourier_watermark(Source, Watermark)
+    
     name = os.path.basename(Source_Filename[order]).split(".")[0]
     save_path = os.path.join(output_path, name + "_de.png")
     print(save_path)
