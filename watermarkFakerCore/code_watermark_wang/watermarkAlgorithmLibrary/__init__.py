5f0ec2aa3e57e632428effb8384f6465d664cb18
Serendipity <hl3265@truman.edu>
2021-07-24 17:58:35 +0800

    Trying let python find this library

---
+++ watermarkFakerCore/code_watermark_wang/watermarkAlgorithmLibrary/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.path.append("watermarkAlgorithmLibrary\base_watermark.py")
+sys.path.append("watermarkAlgorithmLibrary\dct.py")
+sys.path.append("watermarkAlgorithmLibrary\dft.py")
+sys.path.append("watermarkAlgorithmLibrary\lsb.py")
+sys.path.append("watermarkAlgorithmLibrary\lsbm.py")
+sys.path.append("watermarkAlgorithmLibrary\lsbmr.py")
+sys.path.append("watermarkAlgorithmLibrary\rlsb.py")
