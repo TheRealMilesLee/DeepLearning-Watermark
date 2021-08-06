1bd7ace9e03ac2e2385ec3a4cdb6a791dee863ac
Serendipity <hl3265@truman.edu>
2021-08-06 15:09:23 +0800

    Update the file structure

---
+++ watermarkFakerCore/code_watermark_wang/Finished_Algorithm/DCT_Watermark/DCT.py
@@ -0,0 +1,46 @@
+import os
+import cv2
+from watermarkAlgorithmLibrary import dct
+
+if __name__ == '__main__':
+  # 源文件位置
+  source_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DCT/images/Source"
+  watermark_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DCT/images/Watermark/lena.png"
+  output_path = "D:/CS-Related/Watermark Faker/TestFolder/Test_Images/DCT/images/Output"
+  # 统一设置为灰度值
+  RGB = 0
+  # 如果输出的位置文件夹不存在就创建一个新的
+  if not os.path.exists(output_path):
+    os.mkdir(output_path)
+    
+  i = 0
+  for file_name in os.listdir(source_path):
+    # 获取文件夹下的所有文件名
+    image_path = os.path.join(source_path, file_name)
+    # 拆分文件名，以 "." 为切分标志
+    tmp_str = file_name.split(".")[0]
+    # 确定输出的文件名
+    image_wm_path = os.path.join(output_path, tmp_str + "_wDCT.png")
+    # 读取文件
+    image = cv2.imread(image_path, flags = RGB)
+    cv2.imshow('Image', image)
+    cv2.waitKey(0)
+    
+    watermarkLena = cv2.imread(watermark_path, flags = RGB)
+    cv2.imshow('Watermark', watermarkLena)
+    cv2.waitKey(0)
+    
+    ResizeWatermarkHeight = watermarkLena.shape[0] / 8
+    ResizeWatermarkWidth = watermarkLena.shape[1] / 8
+    NewWatermarkImage = (ResizeWatermarkWidth, ResizeWatermarkHeight)
+    
+    resized = cv2.resize(watermarkLena, None, fx = 0.125, fy = 0.125, interpolation = cv2.INTER_AREA)
+    
+    watermarked_image = dct.embed(image, resized)
+    
+    cv2.imwrite(image_wm_path, watermarked_image)
+    
+    i += 1
+    print(i)
+  
+  print("finished")
