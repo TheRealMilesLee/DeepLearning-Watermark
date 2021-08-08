e205732530dbdd3be0977db093db9e8bc25b4fa0
Serendipity <1789216340lhy@gmail.com>
2021-08-08 10:28:36 +0800

    do some comment work

--- watermarkFakerCore/code_watermark_wang/NeedToKnowWhy/lsb_watermark/lsb-cv.py
+++ watermarkFakerCore/code_watermark_wang/NeedToKnowWhy/lsb_watermark/lsb-cv.py
@@ -3,11 +3,16 @@ import numpy as np
 import os
 import glob
 
+#
+input_path = '/Users/arkia/ComputerScienceRelated/watermarkFakerCore/code_watermark_wang/lsb_watermark/input/samples_0.jpg'
+output_path = '/Users/arkia/ComputerScienceRelated/watermarkFakerCore/code_watermark_wang/lsb_watermark/output'
+watermark = cv.imread('/Users/arkia/ComputerScienceRelated/watermarkFakerCore/code_watermark_wang/dft_watermark/lena_color_256.png')
+input_extract = glob.glob(os.path.join(input_path, "*.png"))
 
+"""
+Watermarks the image by replacing the least significant bits of the image.
+"""
 def watermark_lsb(origin, info):
-    """
-    Watermarks the image by replacing the least significant bits of the image.
-    """
     origin = format(origin, '08b')  # 8 bits binary number in str filled by '0' (255 = 2**8)
     info = format(info, '08b')
     wm_msb = info[:2]  # the most significant bits of info
@@ -15,24 +20,14 @@ def watermark_lsb(origin, info):
     result = int(im_lsb + wm_msb, 2)  # turn to decimal integer
     return result
 
-
-input_path = '/Users/arkia/ComputerScienceRelated/watermarkFakerCore/code_watermark_wang/lsb_watermark/input/samples_0.jpg'
-output_path = '/Users/arkia/ComputerScienceRelated/watermarkFakerCore/code_watermark_wang/lsb_watermark/output'
-
+#如果输出目录不存在就创建一个
 if not os.path.exists(output_path):
     os.mkdir(output_path)
 
-input_extract = glob.glob(os.path.join(input_path, "*.png"))
-
-
-
-watermark = cv.imread('/Users/arkia/ComputerScienceRelated/watermarkFakerCore/code_watermark_wang/dft_watermark/lena_color_256.png')
-
 for order in range(len(input_extract)):
+#输出处理的储量
     print(order + 1)
-
     origin = cv.imread(input_extract[order])
-
     # Make watermark image's size as original image
     wmbroad = np.zeros(origin.shape)
     wmbroad[:watermark.shape[0], :watermark.shape[1], :] = watermark
@@ -41,7 +36,7 @@ for order in range(len(input_extract)):
     vec_watermark = np.vectorize(watermark_lsb)  # to apply LSB to matrices and arrays
     watermarked = vec_watermark(origin, wmbroad)
     input_rename = os.path.splitext(os.path.basename(input_extract[order]))[0] + "_wLSB.png"
-
+#输出所有东西
     cv.imwrite(os.path.join(output_path, input_rename), watermarked)
 
 print("finish")
