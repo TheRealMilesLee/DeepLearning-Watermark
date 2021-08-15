90dc41f3e1489f812ce7899c1eed0dd85fc506c2
Serendipity <hl3265@truman.edu>
2021-08-15 10:55:25 +0800

    finished random jitter function

--- watermarkFakerCore/code_watermark_wang/Pix2Pix/Origional/GoogleTensorflow2Version.py
+++ watermarkFakerCore/code_watermark_wang/Pix2Pix/Origional/GoogleTensorflow2Version.py
@@ -3,20 +3,27 @@ import os
 import pathlib
 import time
 import datetime
+import glob
 from matplotlib import pyplot as plt
 from IPython import display
 
+  # The facade training set consist of 400 images
+BUFFER_SIZE = 400
+# The batch size of 1 produced better results for the U-Net in the original pix2pix experiment
+BATCH_SIZE = 1
+# Each image is 256x256 in size
+IMG_WIDTH = 256
+IMG_HEIGHT = 256
 
-#Prepare the file to train for
-dataset_name = "facades"
 # Link to the file
-Source_Directory = '/Users/arkia/ComputerScienceRelated/Watermark Faker/{dataset_name}'
+Source_Directory = 'D:/CS-Related/Watermark Faker/Test_Images/facades/train'
+image_file = glob.glob(os.path.join(Source_Directory,'*.jpg'))
 
+# Load the image
 def load(image_file):
   # Read and decode an image file to a uint8 tensor
   image = tf.io.read_file(image_file)
   image = tf.image.decode_jpeg(image)
-
   # Split each image tensor into two tensors:
   # - one with a real building facade image
   # - one with an architecture label image 
@@ -24,16 +31,45 @@ def load(image_file):
   w = w // 2
   input_image = image[:, w:, :]
   real_image = image[:, :w, :]
-
   # Convert both images to float32 tensors
   input_image = tf.cast(input_image, tf.float32)
   real_image = tf.cast(real_image, tf.float32)
-
   return input_image, real_image
 
-inp, re = load(str(PATH / 'train/100.jpg'))
-# Casting to int for matplotlib to display the images
-plt.figure()
-plt.imshow(inp / 255.0)
-plt.figure()
-plt.imshow(re / 255.0)
+# Crop the image
+def resize(input_image, real_image, height, width):
+  input_image = tf.image.resize(input_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  real_image = tf.image.resize(real_image, [height, width], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  return input_image, real_image
+
+def random_crop(input_image, real_image):
+  stacked_image = tf.stack([input_image, real_image], axis=0)
+  cropped_image = tf.image.random_crop(
+      stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
+  return cropped_image[0], cropped_image[1]
+
+# Normalizing the images to [-1, 1]
+def normalize(input_image, real_image):
+  input_image = (input_image / 127.5) - 1
+  real_image = (real_image / 127.5) - 1
+  return input_image, real_image
+
+@tf.function()
+def random_jitter(input_image, real_image):
+  # Resizing to 286x286
+  input_image, real_image = resize(input_image, real_image, 286, 286)
+  # Random cropping back to 256x256
+  input_image, real_image = random_crop(input_image, real_image)
+  if tf.random.uniform(()) > 0.5:
+    # Random mirroring
+    input_image = tf.image.flip_left_right(input_image)
+    real_image = tf.image.flip_left_right(real_image)
+  return input_image, real_image
+
+plt.figure(figsize=(6, 6))
+for i in range(4):
+  rj_inp, rj_re = random_jitter(inp, re)
+  plt.subplot(2, 2, i + 1)
+  plt.imshow(rj_inp / 255.0)
+  plt.axis('off')
+plt.show()
